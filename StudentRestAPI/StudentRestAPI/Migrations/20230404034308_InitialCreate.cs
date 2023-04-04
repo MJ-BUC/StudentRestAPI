bc83090acf6f8ddae4a3b1c29e779e1f0bda4ed3
MJ-BUC <82846057+MJ-BUC@users.noreply.github.com>
2023-04-04 00:02:44 -0500

    Student REST API using C#

---
+++ StudentRestAPI/StudentRestAPI/Migrations/20230404034308_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentRestAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    departmentId = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath", "departmentId" },
+                values: new object[,]
+                {
+                    { 1, "joe@smaplemail.com", "Joe", 0, "Smith", "Images/joe.png", 1 },
+                    { 2, "john@smaplemail.com", "John", 0, "Smith", "Images/john.png", 2 },
+                    { 3, "james@smaplemail.com", "James", 0, "Johnson", "Images/james.png", 3 },
+                    { 4, "john@smaplemail.com", "John", 0, "Doe", "Images/john.png", 4 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentRestAPI/StudentRestAPI/Models/AppDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRestAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            // Send Student Table
+            modelbuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    StudentId = 1,
+                    FirstName = "Joe",
+                    LastName = "Smith",
+                    Email = "joe@smaplemail.com",
+                    Gender = Gender.Male,
+                    departmentId = 1,
+                    PhotoPath = "Images/joe.png"
+                });
+            modelbuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    StudentId = 2,
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Email = "john@smaplemail.com",
+                    Gender = Gender.Male,
+                    departmentId = 2,
+                    PhotoPath = "Images/john.png"
+                });
+            modelbuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    StudentId = 3,
+                    FirstName = "James",
+                    LastName = "Johnson",
+                    Email = "james@smaplemail.com",
+                    Gender = Gender.Male,
+                    departmentId = 3,
+                    PhotoPath = "Images/james.png"
+                });
+            modelbuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    StudentId = 4,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john@smaplemail.com",
+                    Gender = Gender.Male,
+                    departmentId = 4,
+                    PhotoPath = "Images/john.png"
+                });
+        }
+    }
+}
